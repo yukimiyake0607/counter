f739c15bbacc66cb8ae362a1aa82ce4d53bb4dd6
yukimiyake0607 <yukimiyake38@gmail.com>
2024-07-18 18:12:16 +0900

    refactor: gridView.builderを別ファイルで保管

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:counter/widgets/custom_grid.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,84 +12,7 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: GridView.builder(
-            itemCount: 1,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              childAspectRatio: 1.0,
-            ),
-            itemBuilder: (context, index) {
-              return Container(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      '国語',
-                      style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const Text.rich(
-                        textAlign: TextAlign.center,
-                        TextSpan(children: [
-                          TextSpan(
-                            text: '80',
-                            style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          TextSpan(
-                            text: '分',
-                            style: TextStyle(fontSize: 20, color: Colors.white),
-                          ),
-                        ])),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shape: const CircleBorder(
-                                  side: BorderSide(
-                                color: Colors.black,
-                              )),
-                              minimumSize: Size(40, 40),
-                              padding: EdgeInsets.zero,
-                            ),
-                            child: Icon(Icons.add),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shape: const CircleBorder(
-                                  side: BorderSide(
-                                color: Colors.black,
-                              )),
-                              minimumSize: Size(40, 40),
-                              padding: EdgeInsets.zero,
-                            ),
-                            child: Icon(Icons.remove),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            }),
+        child: CustomGrid(),
       ),
     );
   }
---
+++ lib/widgets/custom_grid.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class CustomGrid extends StatelessWidget {
+  const CustomGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: 1,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 1.0,
+      ),
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                '国語',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              const Text.rich(
+                  textAlign: TextAlign.center,
+                  TextSpan(children: [
+                    TextSpan(
+                      text: '80',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: '分',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ])),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: const CircleBorder(
+                            side: BorderSide(
+                          color: Colors.black,
+                        )),
+                        minimumSize: Size(40, 40),
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: Icon(Icons.add),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: const CircleBorder(
+                            side: BorderSide(
+                          color: Colors.black,
+                        )),
+                        minimumSize: Size(40, 40),
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: Icon(Icons.remove),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
