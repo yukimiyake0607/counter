ba57ed15ce56dbd61e8a105607eac26b4d839976
yukimiyake0607 <yukimiyake38@gmail.com>
2024-07-18 18:07:20 +0900

    add: body部分を追加
    refs 各グリッドアイテムにスタイルを追加

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,8 +9,86 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Counter App'),
       ),
-      body: Center(
-        child: Text('Hello World!'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+            itemCount: 1,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 1.0,
+            ),
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      '国語',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const Text.rich(
+                        textAlign: TextAlign.center,
+                        TextSpan(children: [
+                          TextSpan(
+                            text: '80',
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: '分',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                        ])),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              shape: const CircleBorder(
+                                  side: BorderSide(
+                                color: Colors.black,
+                              )),
+                              minimumSize: Size(40, 40),
+                              padding: EdgeInsets.zero,
+                            ),
+                            child: Icon(Icons.add),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              shape: const CircleBorder(
+                                  side: BorderSide(
+                                color: Colors.black,
+                              )),
+                              minimumSize: Size(40, 40),
+                              padding: EdgeInsets.zero,
+                            ),
+                            child: Icon(Icons.remove),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }),
       ),
     );
   }
